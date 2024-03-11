dd194930602f0978a8c10c670de7e8b78751ebeb
Jack 傑克 108 <dm108Jack@obestsolutions@onmicrosoft.com>
2024-03-11 14:12:59 +0800

    id

--- src/app/Services/Auth/auth.service.ts
+++ src/app/Services/Auth/auth.service.ts
@@ -52,7 +52,7 @@ export class AuthService {
   }
 
   changePassword(id: number, passwordChangeData: PasswordChange): Observable<any> {
-    return this.http.post(`${AuthUrl}PasswordChange?id=${id}`, passwordChangeData, { withCredentials: true })
+    return this.http.post(`${AuthUrl}PasswordChange`, passwordChangeData, { withCredentials: true })
   }
 
 }
